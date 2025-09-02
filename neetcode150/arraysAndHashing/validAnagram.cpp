379ea74e7e80c2b1bc31f211fb9647d3269b1d1e
MohsenKhouaja <mohsenkhouaja3@gmail.com>
2025-09-02 20:31:52 +0100

    validAnagram

---
+++ neetcode150/arraysAndHashing/validAnagram.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+            return false;
+        map<char, int> letters;
+        int n = s.length();
+        for (int i = 0; i < n; i++)
+        {
+            letters[t[i]]--;
+            letters[s[i]]++;
+        }
+        for (const auto elm : letters)
+        {
+            if (elm.second != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
